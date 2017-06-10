57804e073443f78d34e85580cbe30c6d76442e70
Kyle Alexis Sargeant <kasargeant@gmail.com>
2017-06-10 06:33:06 +0200

    Fix to unwanted env variables display.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+/**
+ * @file index.js
+ * @description The module index.
+ * @author Kyle Alexis Sargeant <kasargeant@gmail.com> {@link https://github.com/kasargeant https://github.com/kasargeant}.
+ * @copyright Kyle Alexis Sargeant 2017
+ * @license See LICENSE file included in this distribution.
+ */
+
 // Imports
 const Tinter = require("./js/Tinter");
 
